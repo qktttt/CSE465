966ae0ed39b96fc6656e028aac62219fca69daae
Kunting Qi <qik@miamioh.edu>
2018-10-10 13:42:35 -0400

    first commit

---
+++ qikHw4/pythonCode.py
@@ -0,0 +1,18 @@
+def fillTmp(model, personInfo):
+	pass
+
+def writeInformation(completeInfo, 
+	studentName, outputFilename):
+	pass
+
+def readInfoFile(infoFile):
+	pass
+
+infoFile = open("a.tsv")
+modelFile = open("a.tmp")
+
+# in a loop read the file
+persons = infoFile.readlines()
+for i in person:
+	# seperate information 
+	
