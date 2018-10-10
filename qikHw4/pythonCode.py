125480d381d87ee5b21a392606f64a1e5ea74f42
Qi <qik@miamioh.edu>
2018-10-10 14:26:28 -0400

    developing python code, will be finished later

--- qikHw4/pythonCode.py
+++ qikHw4/pythonCode.py
@@ -1,18 +1,59 @@
-def fillTmp(model, personInfo):
+def fillTmp(modelString, personInfo):
+	resultString = modelString.format(
+		NAME=personInfo["name"], COURSE=personInfo["course"],
+		Id=personInfo["id"], DUE=personInfo["due"], 
+		SUBMITTED=personInfo["submitted"], MINUTESLATE=personInfo["minuteslate"],
+		LATEDEDUCTION=personInfo["latededuction"], P1=personInfo["p1"],
+		P1COMMENTS=personInfo["p1comments"], P2=personInfo["p2"],
+		P2COMMENTS=personInfo["p2comments"], SUBTOTAL=personInfo["subtotal"],
+		TOTAL=personInfo["total"]		
+	) # replace part in model string with customized personal information
+	return resultString
+
+def writeInformation(infoString, outputFilename):
+	#finish writing the file
+	targetFile = open(outputFilename, "w") # open file
+	targetFile.write(infoString)
+	targetFile.close() # close file
+
+def processTmp(modelFilename):
+	totalString = ""
+	modelFile = open(modelFilename) # open file
+	for i in modelFile.readlines():
+		totalString += i
+	modelFile.close() # close file
+	totalString = totalString.replace("<<", "{")
+	totalString = totalString.replace(">>", "}")
+	return totalString
+
+def readInfoFile(infoFilename):
+	""" read the info file, and return a list containing dict
+		:param 
+			infoFile (file object) : the 
+				file object of students information
+		:return
+			list containing dict:
+				[
+					{student1Name: ..., studentCourse: ..., ...} 
+					{student2Name:...... } 
+					{...}
+				]
+	"""
+	# open the file
+	
+	# using for loop to construct the result
+	
+	# close file
+	
+	# return the result
 	pass
+	
+if __name__ == "__main__":
+	fileInfoName = argv[1] # *.tsv file
+	fileModelName = argv[2] # *.tmp file 
 
-def writeInformation(completeInfo, 
-	studentName, outputFilename):
-	pass
-
-def readInfoFile(infoFile):
-	pass
-
-infoFile = open("a.tsv")
-modelFile = open("a.tmp")
-
-# in a loop read the file
-persons = infoFile.readlines()
-for i in person:
-	# seperate information 
+	# in a loop read the file
+	persons = infoFile.readlines()
+	for i in person:
+		# seperate information 
 	
