cb88e58dbedbda67aa3bf993c8b274172209dd04
Qi <qik@miamioh.edu>
2018-11-30 15:59:50 -0500

    start working on hw6

---
+++ hw6/hw6.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace hw6 {
+	class hw6 {
+		public static int Adder(***TODO***)
+		{
+			int sum = 0;
+			
+			***TODO***
+			
+			return sum;
+		}
+		
+		public static int Main(String [] args)
+		{
+			int x = Adder(1,2,3,4,5,6);
+			Console.WriteLine("x = {0}", x);
+		
+			int y = Adder(1,5);
+			Console.WriteLine("y = {0}", y);
+		
+			int z = Adder();
+			Console.WriteLine("z = {0}", z);
+			
+			return 0;
+		}
+	}
+}
---
+++ hw6/hw6.java
@@ -0,0 +1,22 @@
+public class hw6 {
+
+	public static int adder(***TODO***)
+	{
+		int sum = 0;
+		
+		***TODO***
+		
+		return sum;
+	}
+	public static void main(String [] args)
+	{
+		int x = adder(1,2,3,4,5,6);
+		System.out.println("x = " + x);
+		
+		int y = adder(1,5);
+		System.out.println("y = " + y);
+		
+		int z = adder();
+		System.out.println("z = " + z);
+	}
+}
